c9b0444b4a2c73b55bc10a27ce9f078682180333
Calaverd <bfxdosval@gmail.com>
2023-04-25 22:47:53 -0600

    Clean code, removes Camara.

--- libs/mini_core.lua
+++ libs/mini_core.lua
@@ -2,25 +2,23 @@
 function Overloaded()
     local fns = {}
     local mt = {}
-    
+
     local function oerror()
         return error("Invalid argument types to overloaded function")
     end
-    
+
     function mt:__call(...)
         local arg = {...}
         local default = self.default
-        
+
         local signature = {}
         for i,arg in ipairs {...} do
             signature[i] = type(arg)
         end
-        
-        signature = table.concat(signature, ",")
-        
-        return (fns[signature] or self.default)(...)
+
+        return (fns[table.concat(signature, ",")] or self.default)(...)
     end
-    
+
     function mt:__index(key)
         local signature = {}
         local function __newindex(self, key, value)
@@ -36,49 +34,49 @@ function Overloaded()
         end
         return __index(self, key)
     end
-    
+
     function mt:__newindex(key, value)
         fns[key] = value
     end
-    
+
     return setmetatable({ default = oerror }, mt)
 end
 
 --linear interpolation.
-function lerp(a,b,t) return (1-t)*a + t*b end
+local function lerp(a,b,t) return (1-t)*a + t*b end
 
 --[[
 a 2D Vector class 
 ]]
 function vector2D(x,y)
     local self = {x = x or 0,y= y or 0}
-    
+
     function self.magnitud()
       return math.sqrt(self.x*self.x+self.y*self.y) or 0
     end
-    
+
     function self.magnitudCuadrada()
-      return self.x*self.x+self.y*self.y or 0
+      return ((self.x*self.x)+(self.y*self.y)) or 0
     end
-    
+
     function self.productoPunto(vec)
       --print(self.x,self.y)
       --print(vec.x,vec.y)
       return self.x*vec.x + self.y*vec.y
     end
-    
+
     function self.productoCruz(vec)
       --print(self.x,self.y)
       --print(vec.x,vec.y)
       return self.x*vec.y - self.y*vec.x
     end
-    
+
     function self.lerpVector(vec,t)
         local t = t or 0.5
         self.x = lerp(vec.x,self.x,t)
         self.y = lerp(vec.y,self.y,t)
     end
-    
+
     function self.normalizar(min)
       local min = min or 0
       local mag = self.magnitud()
@@ -89,74 +87,74 @@ function vector2D(x,y)
     end
     --sobrecarga de operadores...
     local mt = {
-    __add = function (lhs, rhs) 
+    __add = function (lhs, rhs)
         x = lhs.x + rhs.x
         y = lhs.y + rhs.y
-        return vector2D(x,y) 
+        return vector2D(x,y)
         end,
-    __sub = function (lhs, rhs) 
+    __sub = function (lhs, rhs)
         x = lhs.x - rhs.x
         y = lhs.y - rhs.y
-        return vector2D(x,y) 
+        return vector2D(x,y)
         end,
-    __div = function (lhs, rhs) 
-         if type(rhs) == 'number' then 
+    __div = function (lhs, rhs)
+         if type(rhs) == 'number' then
             x = lhs.x/rhs
             y = lhs.y/rhs
             return vector2D(x,y)
-          end    
+          end
         x = lhs.x/rhs.x
         y = lhs.y/rhs.y
-        return vector2D(x,y) 
+        return vector2D(x,y)
         end,
     __mul = function (lhs,rhs)
-        if type(rhs) == 'number' then 
+        if type(rhs) == 'number' then
             x = lhs.x*rhs
             y = lhs.y*rhs
             return vector2D(x,y)
           end
         x = lhs.x*rhs.x
         y = lhs.y*rhs.y
-        return vector2D(x,y) 
+        return vector2D(x,y)
         end,
     __call = function(a, op)
-        if op == '.' then 
+        if op == '.' then
             return function(b) return self.productoPunto(b) end
         elseif op == 'x' then
             return function(b) return self.productoCruz(b) end
             end
-        
+
         end
-        
+
     }
-    
+
     setmetatable(self, mt) -- use "mt" as the metatable
-    
+
     self.type = 'vector2D'
-    
+
     return self
 end
 
 function Chrono()
     local self = {}
-    local start_time = 0 
+    local start_time = 0
     local pause_time = 0
     local iniciado = false
     local pausado = false
     local detenido = false
-    
+
     function self.estaDetenido()
         return detenido
     end
-    
+
     function self.estaPausado()
         return pausado
     end
-    
+
     function self.estaIniciado()
         return iniciado
     end
-    
+
     function self.start()
         iniciado = true
         pausado = false
@@ -164,7 +162,7 @@ function Chrono()
         start_time = love.timer.getTime()
         pause_time = 0
     end
-    
+
     function self.getDeltaTime()
         if iniciado then
             if pausado then
@@ -175,28 +173,28 @@ function Chrono()
         end
         return 0
     end
-    
+
     function self.getDeltaTimeFrame(max_fps)
         local time = self.getDeltaTime()
         self.start()
         return time
     end
-    
-    function self.hanPasado(segundos)
+
+    function self.hasPassed(segundos)
         if self.getDeltaTime() >=  segundos then
             self.start()
             return true
         end
         return false
     end
-    
+
     function self.pausar()
         if iniciado and (not pausado) then
             pausado = true
             pause_time = love.timer.getTime()-start_time
         end
     end
-    
+
     function self.despausar()
         if iniciado and pausado then
             pausado = false
@@ -204,13 +202,13 @@ function Chrono()
             pause_time = 0
         end
     end
-    
+
     function self.detener()
        detenido = true
        iniciado = false
        pausado = false
     end
-    
+
     return self
 end
 
@@ -219,7 +217,7 @@ function makeQuads(ancho_ima,alto_ima,ancho_r,alto_r)
     local lista = {}
     local filas = math.floor(alto_ima/alto_r)
     local columnas = math.floor(ancho_ima/ancho_r)
-    
+
     offset_x = offset_x or 0
     offset_y = offset_y or 0
 
@@ -240,86 +238,4 @@ function makeQuads(ancho_ima,alto_ima,ancho_r,alto_r)
         --io.write('\n')
     end
     return lista
-end
-
-
-function Camera(x,y)
-    local self = {}
-    self.x = x
-    self.y = y
-    
-    self.drag_x = 0
-    self.drag_y = 0
-    self.drag_ended = true
-    
-    self.move_tween = nil
-    self.move_ended = true
-    
-    self.easing = "linear"
-    
-    function self.getPos()
-        return self.x, self.y
-    end
-    
-    function self.getPosX() return self.x-self.drag_x end
-    function self.getPosY() return self.y-self.drag_y end
-    
-    function self.move(time,nx,ny) 
-        if self.move_ended then
-            self.move_tween = tween.new(time,self, { y = ny, x = nx }, self.easing)
-        end
-    end
-    
-    function self.setTweenEasing(nes)
-        self.easing = nes
-    end
-
-    function self.moveCenterTo(time,nx,ny) 
-        if self.move_ended then
-            self.move(time,nx-320,ny-180)
-        end
-    end
-    
-    function self.isMoving()
-        return not(self.move_ended)
-    end
-    
-    function self.draw()
-        love.graphics.setColor(1,0,1,0.5)
-        love.graphics.circle('fill', self.getPosX(),self.getPosY(), 10)
-        love.graphics.line(self.getPosX(),self.getPosY()+180,self.getPosX()+640,self.getPosY()+180)
-        love.graphics.line(self.getPosX()+320,self.getPosY(),self.getPosX()+320,self.getPosY()+360)
-        love.graphics.setColor(1,1,1,1)
-    end
-    
-    function self.update(dt)
-        if self.move_tween then
-            self.move_ended = (self.move_tween:update(dt))
-        end
-    end
-    
-    --esta arrastrando la camara
-    function self.drag(dx,dy)
-        if self.move_ended then
-            self.drag_x = dx
-            self.drag_y = dy
-            self.drag_ended = false
-        end
-    end
-    
-    function self.setPos(nx,ny)
-        self.move_tween = tween.new( 0.01,self, { y = ny, x = nx }, 'linear')
-    end
-    
-    --la camara deja de arrastrarse...
-    function self.drop(dx,dy)
-        if self.move_ended then
-            self.drag_ended = true
-            self.setPos(self.x-self.drag_x,self.y-self.drag_y)
-            self.drag_x = 0
-            self.drag_y = 0
-        end
-    end
-    
-    return self
 end
